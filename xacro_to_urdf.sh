7d71d8945994a4cf7714531a9cfca1a0e9dbebfb
sond8807-rgb <sond8807@gmail.com>
2026-06-30 15:58:16 -0400

    Update and rename xacro_to_urdf to xacro_to_urdf.sh

--- xacro_to_urdf.sh
+++ xacro_to_urdf.sh
@@ -1,8 +1,8 @@
 "1. Install the xarm5 repo from the following git: https://github.com/xArm-Developer/xarm_ros2/tree/jazzy"
 
 "2. Run the following command in a terminal (make sure to source ros2 before running) to turn it into an urdf"
+
 ros2 run xacro xacro /root/dev_ws/src/xarm_ros2/xarm_description/urdf/xarm_device.urdf.xacro \
   dof:=5 add_gripper:=true \
   -o /root/xarm5_gripper.urdf
 
-  
