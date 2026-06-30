bfb450ad534523b4da717cbfbbf3141ccd01ff41
sond8807-rgb <sond8807@gmail.com>
2026-06-30 16:08:13 -0400

    Update xacro_to_urdf.sh

--- xacro_to_urdf.sh
+++ xacro_to_urdf.sh
@@ -6,8 +6,8 @@ ros2 run xacro xacro /xarm_ros2/xarm_description/urdf/xarm_device.urdf.xacro \
   dof:=5 add_gripper:=true \
   -o /root/xarm5_gripper.urdf
 
-"3. Fix the mesh paths."
+"3. Fix the mesh paths. (you may need to change the destination to the xarm_description folder the after the '|' depending on where you installed it)"
 
 sed -i 's|package://xarm_description|package://xarmros2/xarm_description|g' ~/xarm5_gripper.urdf
 
-"4.
+"4. Open Isaac Sim and import the URDF of the XARM5 into it."
